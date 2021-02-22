8a7fd09f6e93cd6ca9c4430d3f38930ce2fe6cd5
Ntsoccer <2460socceronly0512@gmail.com>
2021-02-22 18:48:20 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -37,44 +37,42 @@
 export default {
   data() {
     return {
-      newItem:'',
-      todos:[],
-      options:[
-        {value:-1,label:'すべて'},
-        {value:0,label:'作業中'},
-        {value:1,label:'完了'}
+      newItem: '',
+      todos: [],
+      options: [
+        {value: -1, label: 'すべて'},
+        {value: 0, label: '作業中'},
+        {value: 1, label: '完了'}
       ],
-      current:-1
+      current: -1
     }
   },
-  methods:{
-    addItem(){
+  methods: {
+    addItem() {
       if(!this.newItem) return
-      const todo={
-        id:this.todos.length,
-        item:this.newItem,
-        state:0
+      const todo = {
+        id: this.todos.length,
+        item: this.newItem,
+        state: 0
       }
       this.todos.push(todo)
-      this.newItem=''
+      this.newItem = ''
     },
-    changeState(item){
-      item.state=item.state?0:1
+    changeState(item) {
+      item.state = item.state ? 0 : 1
     },
-    deleteItem(index){
-      this.todos.splice(index,1)
+    deleteItem(index) {
+      this.todos.splice(index, 1)
     }
   },
-  computed:{
-    filterTodos(){
-      return this.todos.filter(todo=>{
+  computed: {
+    filterTodos() {
+      return this.todos.filter(todo => {
         return this.current < 0 ? true : this.current === todo.state
       },this)
     },
     labels() {
     return this.options.reduce(function(accu, curr) {
-      console.log(accu)
-      console.log(curr)
       return Object.assign(accu, { [curr.value]: curr.label })
     }, {})
   }
